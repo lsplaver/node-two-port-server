073cf90c5d870eb4ccb9b72f516576a14f4ee9fe
unknown <lsplaver@yahoo.com>
2018-04-19 19:28:52 -0400

    negative server working

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var http = require('http');
 
 var handleRequest7000 = function(req, res) {
-    res.end("You are a great coder!");
+    res.end("You have great analytical skills!");
 };
 
 var server7000 = http.createServer(handleRequest7000);
 
 server7000.listen(7000, function() {
     console.log("The server is correctly listening on http://localhost:7000");
+});
+
+var handleRequest7500 = function(req, res) {
+    res.end("You have terrible taste in music!");
+};
+
+var server7500 = http.createServer(handleRequest7500);
+
+server7500.listen(7500, function() {
+    console.log("The server is correctly listening on http://localhost:7500");
 });
